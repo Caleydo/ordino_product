37378b46ab7a853fa0cc9e526fb81bb290820f0f
Holger Stitz <holger.stitz@datavisyn.io>
2020-09-08 14:07:31 +0200

    Conditional cookie bar
    The cookie bar is only visible in EU countries. CircleCI has a different IP address. Hence, the `#cookie-bar-button` is not available. Cypress must react conditionally.

--- cypress/integration/tdp_publicdb/ordino-tours.spec.js
+++ cypress/integration/tdp_publicdb/ordino-tours.spec.js
@@ -44,7 +44,11 @@ context('Ordino Tours', () => {
     cy.clearCookies();
     cy.clearLocalStorage();
 
-    // cy.get('#cookie-bar-button', { timeout: 10000 }).should('be.visible').click();
+    cy.get('#cookie-bar-button').then(($button) => {
+      if($button) {
+        cy.get('#cookie-bar-button').click();
+      }
+    });
 
     cy.get('#loginDialog button').should('be.visible').contains('Login').click();
 
